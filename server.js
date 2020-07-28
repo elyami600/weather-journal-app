c9835c66fb0ed1884043445d741759ddbf011518
Yamil Perez <42903867+elyami600@users.noreply.github.com>
2020-07-27 23:06:38 -0400

    fixed bug

--- server.js
+++ server.js
@@ -47,19 +47,19 @@ app.post('/addData', addWeather);
 
 function addWeather(req,res) {
     console.log("req.body ~> ",req.body)
-    if(req.body) {
-        console.log("got here")
-        projectData = req.body;
-    }
-    // projectData.data = req.body.data;
-    // projectData.temp = req.body.temp;
-    // projectData.feel = req.body.feel;
+    // if(req.body) {
+    //     console.log("got here")
+    //     projectData = req.body;
+    // }
+    projectData.data = req.body.data;
+    projectData.temp = req.body.temp;
+    projectData.feel = req.body.feel;
 
     // projectData["date"] = req.body.date;
     // projectData["temp"] = req.body.temp;
     // projectData["feel"] = req.body.feel;
     
-    // res.send({wepa: 100})
+    res.send(projectData)
     console.log("projectData ~>",projectData)
 
 }
